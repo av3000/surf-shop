f64da7fa66a8b2f4b610dfce77c387cf213c1482
av3000 <32328508+av3000@users.noreply.github.com>
2018-08-02 08:22:03 +0300

    Add model files

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema   = mongoose.Schema;
+
+const PostSchema = new Schema({
+	title: String,
+	price: String,
+	description: String,
+	images: [ String ],
+	location: String,
+	lat: Number,
+	lng: Number,
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Review'
+		}
+	]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Post', PostSchema);
+
+/*
+Post
+- title - string
+- price - string 
+- description - string
+- images - array of strings 
+- location - string
+- lat - number
+- lng - number
+- author - object id (ref User)
+- reviews - array of objects
+*/
